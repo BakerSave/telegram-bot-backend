271bf0fe8182e06582e0d2ee682e05f8ddb34e1a
pekal <pekalekar@mail.ru>
2025-05-06 14:46:07 +0300

    Fix app init and add static support

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
-
-from fastapi.responses import HTMLResponse
 
 @app.get("/message", response_class=HTMLResponse)
 def get_message(text: str):
     return f"""
     <html>
-        <body>
-            <h2>Вы написали: {text}</h2>
-            <img src="/static/cat.jpg" width="300">
-        </body>
+      <body>
+        <h2>Вы написали: {text}</h2>
+        <img src="/static/cat.jpg" width="300">
+      </body>
     </html>
     """
