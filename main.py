5f39834884f376bde82a9701470b21b5828b5d38
pekal <pekalekar@mail.ru>
2025-05-06 14:33:04 +0300

    Fix image path

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi.staticfiles import StaticFiles
 
-app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-@app.get("/message")
+from fastapi.responses import HTMLResponse
+
+@app.get("/message", response_class=HTMLResponse)
 def get_message(text: str):
-    return PlainTextResponse(f"Вы написали: {text}")
+    return f"""
+    <html>
+        <body>
+            <h2>Вы написали: {text}</h2>
+            <img src="/static/image.jpg" width="300">
+        </body>
+    </html>
+    """
